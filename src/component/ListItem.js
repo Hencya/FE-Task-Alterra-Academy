55f7c4819ec3eb1ab9f69b254e3543261ee53535
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-11-18 08:26:33 +0700

    push task react graphql mutation

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,14 +1,22 @@
 import "./Home.css";
+import PassengerEdit from "./PassengerEdit";
 const ListItem = (props) => {
   const { id, nama, umur, Jenis_Kelamin } = props.data;
 
   return (
     <tr>
+      <td>{id}</td>
       <td>{nama}</td>
       <td>{umur}</td>
       <td>{Jenis_Kelamin}</td>
       <td className="removeBorder" onClick={() => props.hapusPengunjung(id)}>
         <button>Hapus</button>
+      </td>
+      <td className="removeBorder">
+        <PassengerEdit
+          id={id}
+          editPengunjung={props.editPengunjung}
+        ></PassengerEdit>
       </td>
     </tr>
   );
--- src/component/ListPassenger.js
+++ src/component/ListPassenger.js
@@ -2,22 +2,21 @@ import ListItem from "./ListItem";
 const ListPassenger = (props) => {
   return (
     <div>
-      <hr />
-      <input value={props.userId} onChange={props.onChangeUserId} />
-      <button onClick={props.onGetData}>Get Data</button>
-      <hr />
+      <br />
       <table cellPadding="5px" cellSpacing="0" style={{ margin: "auto" }}>
         <thead bgcolor="red">
+          <td>id</td>
           <td>Nama</td>
           <td>Umur</td>
           <td>Jenis Kelamin</td>
           <td bgcolor="white" className="removeBorder"></td>
         </thead>
-        {props.data?.task_21_table_user.map((item) => (
+        {props.passenger?.map((item) => (
           <ListItem
             key={item.id}
             data={item}
             hapusPengunjung={props.hapusPengunjung}
+            editPengunjung={props.editPengunjung}
           />
         ))}
       </table>
