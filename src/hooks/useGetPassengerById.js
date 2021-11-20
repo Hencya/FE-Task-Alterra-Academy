02bc1e0cd51fc1dadc4b011fe3ea41dba82cc15f
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-11-20 23:00:40 +0700

    fix bug

--- src/hooks/useGetPassengerById.js
+++ src/hooks/useGetPassengerById.js
@@ -3,10 +3,10 @@ import { GetPassengerByID } from "../graphql/query";
 
 export default function useGetPassengerByID(state) {
   const [
-    getPassenger,
-    { data: dataById, loading: loadingSearch, error: errorSearch },
+    getPassengerByid,
+    { data: dataById, loading: loadingById, error: errorById },
   ] = useLazyQuery(GetPassengerByID, {
     fetchPolicy: "network-only",
   });
-  return { dataById, getPassenger, loadingSearch, errorSearch };
+  return { dataById, getPassengerByid, loadingById, errorById };
 }
